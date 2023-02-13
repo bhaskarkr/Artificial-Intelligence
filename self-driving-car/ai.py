50346230bea084b7c5fc876890efbf687777e9da
bhaskarkr <officialbhaskarkumar@gmail.com>
2023-02-12 21:23:00 -0700

    Add : ai.py : class Network : forward function

--- self-driving-car/ai.py
+++ self-driving-car/ai.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import random
 import os
@@ -9,10 +8,18 @@ import torch.optim as optim
 import torch.autograd as autograd
 from torch.autograd import Variable
 
+
 class Network(nn.Module):
 
     def __init__(self, number_input_neuron, number_output_neuron_actions):
         super(Network, self).__init__()
         self.number_input_neuron = number_input_neuron
         self.number_output_neuron_actions = number_output_neuron_actions
+        number_of_neuron_in_hidden_layer = 30
+        self.fullConnection1 = nn.Linear(number_input_neuron, number_of_neuron_in_hidden_layer)
+        self.fullConnection2 = nn.Linear(number_of_neuron_in_hidden_layer, number_output_neuron_actions)
 
+    def forward(self, state):
+        x = F.relu(self.fullConnection1(state))
+        q_values = self.fullConnection2(x)
+        return q_values
